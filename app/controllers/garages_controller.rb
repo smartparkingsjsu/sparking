fc78270830cd3031dc2a567293409f8c699f8948
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-09-11 13:27:16 -0700

    added garage
    lat and long for maps included

---
+++ app/controllers/garages_controller.rb
@@ -0,0 +1,74 @@
+class GaragesController < ApplicationController
+  before_action :set_garage, only: [:show, :edit, :update, :destroy]
+
+  # GET /garages
+  # GET /garages.json
+  def index
+    @garages = Garage.all
+  end
+
+  # GET /garages/1
+  # GET /garages/1.json
+  def show
+  end
+
+  # GET /garages/new
+  def new
+    @garage = Garage.new
+  end
+
+  # GET /garages/1/edit
+  def edit
+  end
+
+  # POST /garages
+  # POST /garages.json
+  def create
+    @garage = Garage.new(garage_params)
+
+    respond_to do |format|
+      if @garage.save
+        format.html { redirect_to @garage, notice: 'Garage was successfully created.' }
+        format.json { render :show, status: :created, location: @garage }
+      else
+        format.html { render :new }
+        format.json { render json: @garage.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /garages/1
+  # PATCH/PUT /garages/1.json
+  def update
+    respond_to do |format|
+      if @garage.update(garage_params)
+        format.html { redirect_to @garage, notice: 'Garage was successfully updated.' }
+        format.json { render :show, status: :ok, location: @garage }
+      else
+        format.html { render :edit }
+        format.json { render json: @garage.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /garages/1
+  # DELETE /garages/1.json
+  def destroy
+    @garage.destroy
+    respond_to do |format|
+      format.html { redirect_to garages_url, notice: 'Garage was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_garage
+      @garage = Garage.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def garage_params
+      params.require(:garage).permit(:name, :longitude, :latitude)
+    end
+end
---
+++ app/views/garages/_garage.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! garage, :id, :name, :longitude, :latitude, :created_at, :updated_at
+json.url garage_url(garage, format: :json)
