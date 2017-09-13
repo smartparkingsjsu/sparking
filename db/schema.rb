3019901bc575e2b5c39f4c980bd52d83806c5939
Alexis <alexischeungho@gmail.com>
2017-09-12 21:25:58 -0700

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170913003107) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "license_plate"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -45,8 +47,13 @@ ActiveRecord::Schema.define(version: 20170913003107) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
