5583b5e4effd11b59ffa3a9e2a6a3b0cafee975b
cheriemeiyee <cheriemeiyee@gmail.com>
2017-09-12 18:54:25 -0700

    extra stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  
   resources :garage_spots
   resources :spots
   resources :garages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "home" => "home#show"
 
   root "home#show", page: "home"
 end
