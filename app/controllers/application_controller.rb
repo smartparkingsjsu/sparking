7fa3e61d9ca8fa796fbc5246052a398662712cf2
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-09-17 09:07:59 -0700

    defined custom methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :super_admin?, :garage_owner?
+
+  def super_admin?
+    if current_user.present? && current_user.admin? && current_user.garage_id.nil?
+      return true
+    else
+      return false
+    end
+  end
+
+  def garage_owner?
+    if current_user.present? && current_user.admin? && current_user.garage_id.present?
+      return current_user.garage_id
+    else
+      return false
+    end
+  end
 
 end
