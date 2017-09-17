3b7c07cc41101d26937c48d24168c9af48297d91
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-09-16 22:52:37 -0700

    added admin
    pending custom helper

--- db/schema.rb
+++ db/schema.rb
@@ -47,25 +47,15 @@ ActiveRecord::Schema.define(version: 20170914002558) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "license_plate"
+    t.boolean "admin", default: false, null: false
+    t.integer "garage_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["garage_id"], name: "index_users_on_garage_id"
+    t.index ["license_plate"], name: "index_users_on_license_plate", unique: true
   end
 
 end
