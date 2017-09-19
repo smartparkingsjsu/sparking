a13baed4bc926aedf7aa3d2873aa4b09dbb8e2c9
Alexis <alexischeungho@gmail.com>
2017-09-18 22:25:12 -0700

    Improve forms to match materialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :events
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, controllers:  { sessions: 'users/sessions'}
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, controllers:  { sessions: 'users/sessions', registrations: 'users/registrations'}
 
   resources :garage_spots
   resources :spots
