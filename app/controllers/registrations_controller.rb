0c3df083c111e31dd800a11c85d25fff36b4ed07
Alexis <alexischeungho@gmail.com>
2017-09-24 21:47:58 -0700

    Add phone mask for input text

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
-    
-      private
-    
-      def sign_up_params
-        params.require(:user).permit(:name, :email, :phone, :password, :password_confirmation)
-      end
-    
-      def account_update_params
-        params.require(:user).permit(:name, :email, :phone, :password, :password_confirmation, :current_password)
-      end
-    end
+
+  def update
+    super
+    resource.update_attribute(:phone, params[:user][:phone].gsub(/\D/, ''))
+  end
+
+  protected
+
+  def after_update_path_for(resource)
+    user_path(@user.id)
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :phone, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :phone, :password, :password_confirmation, :current_password)
+  end
+
+end
