f48dd4db72e21c3256d02c2c657c2927cd2d33d8
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-09-25 16:39:27 -0700

    fixed license plate

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::BookingsController < ApplicationController
     
     def index
-        @booking = Booking.joins(:user).pluck(:id, :license_plate, :start_time, :end_time, :garage_spot_id)
+        @booking = Booking.joins(:user).pluck(:id, :start_time, :end_time, :garage_spot_id)
         render json: @booking
     end
 
