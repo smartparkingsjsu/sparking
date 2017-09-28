54cc81fb44b28dc973173725ac9d13ef646ec819
Alexis <alexischeungho@gmail.com>
2017-09-28 21:26:53 +0000

    Add ultrasonic & base classes

---
+++ edison/wrapper/sensor.py
@@ -0,0 +1,12 @@
+class Sensor(object):
+
+    def __init__(self):
+        self.name = None
+        self.port = None
+        self.type = None
+
+    def write(self):
+        pass
+
+    def read(self):
+        pass
