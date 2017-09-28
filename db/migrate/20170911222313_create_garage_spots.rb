6e84d371d2c99e4b38319112b68af9777fdd143d
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-09-28 08:50:09 -0700

    fix typo

--- db/migrate/20170911222313_create_garage_spots.rb
+++ db/migrate/20170911222313_create_garage_spots.rb
@@ -3,7 +3,7 @@ class CreateGarageSpots < ActiveRecord::Migration[5.1]
     create_table :garage_spots do |t|
       t.references :garage, foreign_key: true
       t.references :spot, foreign_key: true
-      t.string :serial, 
+      t.string :serial
 
       t.timestamps
     end
