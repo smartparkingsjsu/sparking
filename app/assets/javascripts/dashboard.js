f73c42571b4a5adf4ce6cb4d7a720a6b9bfd717d
Alexis <alexischeungho@gmail.com>
2017-10-15 23:20:26 -0700

    Add booking table to dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 
-    // Change timer to 30 mins, 5 secs right now
-    gon.watch("bookings", {interval: 5000}, renewBookings);
+    // Change timer to 30 mins, 5 secs right now for debugging
+    gon.watch("bookings", {interval: 5000}, renewTable);
 
-    var bookingsLength = gon.bookings.length
+    function renewTable(newVal) {
+        response = newVal;
 
-    function renewBookings(newVal) {
-        $("#swipe-1").text(newVal.length);
+        $("#table_contents").empty();
+
+        $.each(response, function(i, item) {
+            var $tr = $('<tr>').append(
+                $('<td>').text(item.id),
+                $('<td>').text(item.start),
+                $('<td>').text(item.end),
+                $('<td>').text(item.recurring),
+                $('<td>').text(item.allDay)
+            ).appendTo('#table_contents');
+        });
+
+        $("#table_contents").load();
     }
 
 });
