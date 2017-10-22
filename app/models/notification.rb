e43647680f464180cdb80bf09fa6d957967297f2
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-10-22 10:31:01 -0700

    initial testing

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,10 @@
 class Notification < ApplicationRecord
     belongs_to :recipient, class_name: "User"
     belongs_to :booking
+    validates :recipient_id, presence: true
+    validates :confidence, presence: true
+    validates :booking_id, presence: true
+    validates :action, presence: true
 
     scope :unread, -> {where(read_at: nil)}
 end
--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SpotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save spot without name" do
+    spot = Spot.new
+    assert_not spot.save
+  end
+
 end
