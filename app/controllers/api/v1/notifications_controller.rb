150f9e88428fbde49ea17e08e81d7f4c621f2213
Alexis <alexischeungho@gmail.com>
2017-10-28 23:56:21 -0700

    Fix json format on API

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -3,13 +3,46 @@ class API::V1::NotificationsController < ApplicationController
 
   def index
     @notifications = Notification.joins(booking: {garage_spot: :garage}).joins(booking: {garage_spot: :spot}).pluck("notifications.id, booking_id, read_at, notifications.created_at, action, confidence, garages.name, spots.name")
-    render json: @notifications, status: :ok
+
+    # Form json
+    notifications_array = []
+    @notifications.each do |notification|
+      temp_json = {
+          "notification_id": notification[0],
+          "booking_id": notification[1],
+          "read_at": notification[2],
+          "created_at": notification[3],
+          "message": notification[4],
+          "confidence": notification[5],
+          "garage_name": notification[6],
+          "spot_name": notification[7]
+      }
+      notifications_array.push(temp_json)
+    end
+
+    render json: {"notifications": notifications_array}, status: :ok
   end
 
   def show
     @notifications = Notification.joins(booking: {garage_spot: :garage}).joins(booking: {garage_spot: :spot}).where("garages.id = ?", params[:id]).pluck("notifications.id, booking_id, read_at, notifications.created_at, action, confidence, garages.name, spots.name")
-            
-    render json: @notifications, status: :ok
+
+    # Form json
+    notifications_array = []
+    @notifications.each do |notification|
+      temp_json = {
+          "notification_id": notification[0],
+          "booking_id": notification[1],
+          "read_at": notification[2],
+          "created_at": notification[3],
+          "message": notification[4],
+          "confidence": notification[5],
+          "garage_name": notification[6],
+          "spot_name": notification[7]
+      }
+      notifications_array.push(temp_json)
+    end
+
+    render json: {"notifications": notifications_array}, status: :ok
   end
 
   def create
