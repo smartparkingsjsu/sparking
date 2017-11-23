62272540a1d71d555b14ab03c45da356f6dc44a3
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-11-23 10:48:48 -0800

    trying to fix timeout

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,22 +1,14 @@
+require 'redis'
+
+REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+
 Geocoder.configure(
-  # Geocoding options
-  timeout: 6                 # geocoding service timeout (secs)
-  # lookup: :google,            # name of geocoding service (symbol)
-  # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
-  # use_https: false,           # use HTTPS for lookup requests? (if supported)
-  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-
-  # Exceptions that should not be rescued by default
-  # (if you want to implement custom error handling);
-  # supports SocketError and Timeout::Error
-  # always_raise: [],
-
-  # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
-  # distances: :linear          # :spherical or :linear
+  lookup: :google,
+  cache: REDIS,
+  always_raise: [
+    Geocoder::OverQueryLimitError,
+    Geocoder::RequestDenied,
+    Geocoder::InvalidRequest,
+    Geocoder::InvalidApiKey
+  ]
 )
